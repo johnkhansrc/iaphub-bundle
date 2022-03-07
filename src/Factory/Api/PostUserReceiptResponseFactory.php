22028736318f840d1686b338976caa68308d4b1b
joHNkHaNsrc <jonathanmoreldev@gmail.com>
2022-03-07 16:54:48 +0100

    chore: remove dump

--- src/Factory/Api/PostUserReceiptResponseFactory.php
+++ src/Factory/Api/PostUserReceiptResponseFactory.php
@@ -11,20 +11,15 @@ class PostUserReceiptResponseFactory
      */
     public static function build(array $data): PostUserReceiptResponse
     {
-        try {
-
-            $newTransactions = [];
-            foreach ($data['newTransactions'] as $transactionData) {
-                $newTransactions[] = TransactionFactory::build($transactionData);
-            }
-            $oldTransactions = [];
-            foreach ($data['oldTransactions'] as $transactionData) {
-                $oldTransactions[] = TransactionFactory::build($transactionData);
-            }
-
-            return new PostUserReceiptResponse($data['status'], $newTransactions, $oldTransactions);
-        } catch (\Exception $exception) {
-            dd($data, $exception);
+        $newTransactions = [];
+        foreach ($data['newTransactions'] as $transactionData) {
+            $newTransactions[] = TransactionFactory::build($transactionData);
         }
+        $oldTransactions = [];
+        foreach ($data['oldTransactions'] as $transactionData) {
+            $oldTransactions[] = TransactionFactory::build($transactionData);
+        }
+
+        return new PostUserReceiptResponse($data['status'], $newTransactions, $oldTransactions);
     }
 }
